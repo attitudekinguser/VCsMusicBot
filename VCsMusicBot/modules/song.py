08026dcc60cf862fa0ef6bdf22fcad73403e1ed6
Attitude King <90306152+attitudekinguser@users.noreply.github.com>
2021-11-02 18:04:27 +0530

    Update song.py

--- VCsMusicBot/modules/song.py
+++ VCsMusicBot/modules/song.py
@@ -33,7 +33,7 @@ def song(client, message):
     for i in message.command[1:]:
         query += " " + str(i)
     print(query)
-    m = message.reply("🔎 Finding the song...")
+    m = message.reply("🔎 Finding song from @attitude_galaxy...")
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
@@ -59,7 +59,7 @@ def song(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = "**🎵 Uploaded by @VCsMusicBot**"
+        rep = "**🎵 Uploaded by @Op_moti_music_robot**"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
